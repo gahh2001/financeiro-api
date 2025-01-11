9bef905d1d29c368b64793b1ecbc41dbe7968b6d
gabriel.goulart <gabriel.goulart@syonet.com>
2025-01-11 10:53:20 -0300

    ajustes

--- src/main/java/org/financeiro/repository/CategoriaMovimentacaoRepository.java
+++ src/main/java/org/financeiro/repository/CategoriaMovimentacaoRepository.java
@@ -24,14 +24,14 @@ public class CategoriaMovimentacaoRepository
 	@Override
 	@Transactional
 	public List<CategoriaMovimentacao> listaCategoriasMovimentacaoPorConta(String googleId) {
-		return list("select t from CategoriaMovimentacao t where t.googleId = ?1", googleId);
+		return list("select t from CategoriaMovimentacao t where t.googleId = ?1 order by t.nomeCategoria asc", googleId);
 	}
 
 	@Override
 	@Transactional
 	public CategoriaMovimentacao listaCategoriaMovimentacaoPorId(Long idCategoria, String googleId) {
 		List<CategoriaMovimentacao> categoria = list ("select c from CategoriaMovimentacao c "
-			+ "where c.id = ?1 and c.googleId = ?2", idCategoria, googleId);
+			+ "where c.id = ?1 and c.googleId = ?2 order by c.nomeCategoria asc", idCategoria, googleId);
 		return categoria != null && !categoria.isEmpty() ? categoria.get(0) : null;
 	}
 
@@ -41,7 +41,8 @@ public class CategoriaMovimentacaoRepository
 			String googleId) {
 		return "TODOS".equals(tipoMovimentacao)
 			? this.listaCategoriasMovimentacaoPorConta( googleId )
-			: list("select t from CategoriaMovimentacao t where t.tipoMovimentacao = ?1 and t.googleId = ?2",
+			: list("select t from CategoriaMovimentacao t where t.tipoMovimentacao = ?1 "
+				+ "and t.googleId = ?2 order by t.nomeCategoria asc",
 				tipoMovimentacao, googleId);
 	}
 
